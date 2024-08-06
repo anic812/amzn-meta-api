190c9613d269ed3d8317c54545b60cb9b844d10f
anic812 <167546532+anic812@users.noreply.github.com>
2024-08-06 21:24:51 +0530

    Add files via upload

---
+++ contr.py
@@ -0,0 +1,237 @@
+COUNTRIES = {
+    "AE": {
+        "continent": "EU",
+    },
+    "AR": {
+        "continent": "NA",
+        "master_keys": {
+            "KATANA_CONTENT": "f141713983de9d7a7097282c86d4760573f34a4c324b4c0e0d6e429b8c9f6198",
+            "HAWKFIRE_CONTENT": "daa499e34758ec097b7ddb77fd5205fbd62364d975f72b482979660ed8ca344c",
+        },
+    },
+    "AT": {
+        "continent": "EU",
+        "suffix": "de",
+    },
+    "AU": {
+        "continent": "FE",
+        "suffix": "com.au",
+        "master_keys": {
+            "KATANA_CONTENT": "43193aa98c3f23040ddfba710dcc121a65261c4ce2d9eb1b32afd2e907a6c0cb",
+        },
+    },
+    "BE": {
+        "continent": "EU",
+    },
+    "BG": {
+        "continent": "EU",
+    },
+    "BO": {
+        "continent": "NA",
+    },
+    "BR": {
+        "continent": "NA",
+        "suffix": "com.br",
+        "master_keys": {
+            "KATANA_CONTENT": "09170a185590a8182e0623f5a106c45fe02e5ffca806363bcbb33a91692b4355",
+            "HAWKFIRE_CONTENT": "8517d79a6ccb68e46f220a7b70b3d7a4e21643d6817c5e2f25058d882cdd794d",
+        },
+    },
+    "CA": {
+        "continent": "NA",
+        "suffix": "ca",
+        "master_keys": {
+            "KATANA_CONTENT": "5e09010ef260436613cbb1c7ba295ff35a0c10bf703fb700a82766cdc43fa239",
+        },
+    },
+    "CH": {
+        "continent": "EU",
+        "suffix": "de",
+    },
+    "CL": {
+        "continent": "NA",
+    },
+    "CO": {
+        "continent": "NA",
+    },
+    "CR": {
+        "continent": "NA",
+        "master_keys": {
+            "KATANA_CONTENT": "5f0fee6f539dce589ed2ccd93793775016a3df9c5cc7065548236d41b4bc7ba1",
+            "HAWKFIRE_CONTENT": "1ddd3e5339cef980bfb7a1c16fe66a2737895d7c0d57a0e37c8acc55b26f9051",
+        },
+    },
+    "CY": {
+        "continent": "EU",
+    },
+    "CZ": {
+        "continent": "EU",
+    },
+    "DE": {
+        "continent": "EU",
+        "suffix": "de",
+        "master_keys": {
+            "KATANA_CONTENT": "9f6e8f3815c2b97db7b53275ab3e7cd971e31b54780cab5670397496f4fb84fa",
+        },
+    },
+    "DO": {
+        "continent": "NA",
+    },
+    "EC": {
+        "continent": "NA",
+    },
+    "EE": {
+        "continent": "EU",
+    },
+    "EG": {
+        "continent": "EU",
+    },
+    "ES": {
+        "continent": "EU",
+        "suffix": "es",
+        "master_keys": {
+            "KATANA_CONTENT": "8239cf6d2583555f2121f34ee59ccd6b0cb0c09e20e48c8d8ca810a6f8d4bd31",
+            "HAWKFIRE_CONTENT": "7c2884ea18f4618810878d5e515848362d8cef5345ac4a528b27ee7047745b4e",
+        },
+    },
+    "FI": {
+        "continent": "EU",
+    },
+    "FR": {
+        "continent": "EU",
+        "suffix": "fr",
+        "master_keys": {
+            "KATANA_CONTENT": "6f3a83c7726096c28f6a22583e972c4d3c1b00857ed3f72a830eeffc8c1fcc8b",
+            "HAWKFIRE_CONTENT": "57b49aa73a52c93a6c2d8b30abcea618ba788abda9e989dfa4506dc4aa316b2f",
+        },
+    },
+    "GB": {
+        "continent": "EU",
+        "suffix": "co.uk",
+        "master_keys": {
+            "KATANA_CONTENT": "1aacd251c5f0ca18afbe35b4e904353d28b4dd755608a8e421639908333f6ae3",
+        },
+    },
+    "GR": {
+        "continent": "EU",
+    },
+    "GT": {
+        "continent": "NA",
+    },
+    "HN": {
+        "continent": "NA",
+    },
+    "HU": {
+        "continent": "EU",
+    },
+    "IE": {
+        "continent": "EU",
+    },
+    "IN": {
+        "continent": "EU",
+        "suffix": "in",
+        "master_keys": {
+            "ROBIN_CONTENT": "864f3369d26f0fcf62f8a7bdcea66f7d33c9d40da432cbc5cfdbeda1da930a59",
+        },
+    },
+    "IS": {
+        "continent": "EU",
+    },
+    "IT": {
+        "continent": "EU",
+        "suffix": "it",
+        "master_keys": {
+            "KATANA_CONTENT": "44771e4ebaa85ea1fd7c8af4ede2f8c84c2a7c8b98347f946a65bffca6a86bc5",
+            "HAWKFIRE_CONTENT": "526a5ecc5f1d31790cd65aaf71e3512885a4ab2dc2d3f6d2b4ac1bc6e882b82f",
+        },
+    },
+    "JP": {
+        "continent": "FE",
+        "suffix": "co.jp",
+        "master_keys": {
+            "KATANA_CONTENT": "5a916c21e568ae84757929aa1dc922476ac7db8ef778b490f6c03621d5d9cce0",
+            "HAWKFIRE_CONTENT": "8a0c3588d786a88e0ca36d25ee8c2e41aab60ed410caeddb3d8f6cc051edda8a",
+        },
+    },
+    "LI": {
+        "continent": "EU",
+    },
+    "LT": {
+        "continent": "EU",
+    },
+    "LU": {
+        "continent": "EU",
+    },
+    "LV": {
+        "continent": "EU",
+    },
+    "MT": {
+        "continent": "EU",
+    },
+    "MX": {
+        "continent": "NA",
+        "suffix": "com.mx",
+        "master_keys": {
+            "KATANA_CONTENT": "f10ddd9aed0fea9a11537b5532b243600c3f788bfa5c14a9aea5225ed820bc19",
+        },
+    },
+    "NI": {
+        "continent": "NA",
+    },
+    "NL": {
+        "continent": "EU",
+    },
+    "NZ": {
+        "continent": "FE",
+        "suffix": "com.au",
+        "master_keys": {
+            "KATANA_CONTENT": "46b7febf9cf48cc9300feb825eae1f953a2629a1719a1250256ec37b69064825",
+            "HAWKFIRE_CONTENT": "cf8b30d77df7934dd4fb4da119d428b278838facbb0987e3a76807f559e38daa",
+        },
+    },
+    "PA": {
+        "continent": "NA",
+    },
+    "PE": {
+        "continent": "NA",
+    },
+    "PL": {
+        "continent": "EU",
+    },
+    "PT": {
+        "continent": "EU",
+        "master_keys": {
+            "KATANA_CONTENT": "a25f5cd72dcb1823141bd3984c0fed16b34678a2e989f38b2096fc3c35a8cf37",
+            "HAWKFIRE_CONTENT": "aff6253b38bb417fa71e6c3ed2a44aedf1e6f15772bbf970255abfbe00d113c",
+        },
+    },
+    "PY": {
+        "continent": "NA",
+    },
+    "SA": {
+        "continent": "EU",
+    },
+    "SE": {
+        "continent": "EU",
+    },
+    "SK": {
+        "continent": "EU",
+    },
+    "SV": {
+        "continent": "NA",
+    },
+    "UK": {
+        "continent": "EU",
+        "suffix": "co.uk",
+    },
+    "US": {
+        "continent": "NA",
+        "master_keys": {
+            "KATANA_CONTENT": "47ba7dc1f2e1745f535d5ba85368da09ff50fd900c45b5112a65a7b13128f2f9",
+            "SONIC_RUSH_CONTENT": "737666ec92c1c52a1ff03fc0f84dde8a8e1fa863f94b9981904686633f8a0ee2",
+        },
+    },
+    "UY": {
+        "continent": "NA",
+    },
+};
